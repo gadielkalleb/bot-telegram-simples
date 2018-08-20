b717f37c6332c62715e4aa5cb00c2dcb9e45eedc
Gadiel Kalleb <gadiel_kalleb@hotmail.com>
2018-08-20 11:57:06 -0300

    colocando o controller de usuario no index

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const Telebot = require('telebot');
 const path = require('path');
 const bot = new Telebot(process.env.BOT_TOKEN);
 
-require(path.resolve('./bot'))(bot);
+bot.on('text', (msg) => {
+  if (msg.chat.username !== process.env.USERNAME && msg.from.id !== process.env.ID) {
+		bot.sendMessage(msg.from.id, 'Você não é meu criador não vou atender suas solicitações')
+  } else {
+    require(path.resolve('./bot'))(bot, msg);
+  }
+}
 
 bot.start();
