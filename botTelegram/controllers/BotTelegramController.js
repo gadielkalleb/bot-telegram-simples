7c50d244decea082e0df8d67f94ac51e0d463f26
gadielkalleb <gadiel_kalleb@hotmail.com>
2018-12-03 08:38:29 -0200

    revisão do projeto

--- botTelegram/controllers/BotTelegramController.js
+++ botTelegram/controllers/BotTelegramController.js
@@ -1,5 +1,6 @@
 const Telebot = require('telebot');
 const witClient = require('../../wit')
+let payload
 
 class BotTelegram {
   constructor(token, db) {
@@ -26,30 +27,24 @@ class BotTelegram {
   }
 
   exibir() {
-    this.Bot.on('/exibir', msg => {
-      if (msg.chat.username !== process.env.USERNAME && msg.chat.id !== process.env.ID) {
-        this.Bot.sendMessage(msg.from.id, 'Você não é meu criador não vou atender suas solicitações')
-        return false
-      } else {
-        this.db
-          .find()
-          .sort({ createdAt: -1 })
-          .limit(6)
-          .then(async gastos => {
-            if (gastos.length <= 0 ) {
-              this.Bot.sendMessage(msg.from.id, 'Nenhum valor encontrado')
-            }
-            for (let i = 0; i < gastos.length; i++) {
-              let li = gastos[i]
-              await this.Bot.sendMessage(msg.from.id, `O valor R$${li.valor}, foi gasto em ${li.descricao} no dia ${li.criadoEm}`)
-            }
-          })
-          .catch (error => {
-            console.log('error: ',error)
-            this.Bot.sendMessage(msg.from.id, 'não foi possivel retornar a lista de gastos')
-        }) 
-      }
-    })
+    this.db
+      .find()
+      .sort({ createdAt: -1 })
+      .limit(6)
+      .then(async gastos => {
+        if (gastos.length <= 0 ) {
+          this.Bot.sendMessage(msg.from.id, 'Nenhum valor encontrado')
+        }
+        for (let i = 0; i < gastos.length; i++) {
+          let li = gastos[i]
+          await this.Bot.sendMessage(msg.from.id, `O valor R$${li.valor}, foi gasto em ${li.descricao} no dia ${li.criadoEm}`)
+        }
+      })
+      .catch (error => {
+        console.log('error: ',error)
+        this.Bot.sendMessage(msg.from.id, 'não foi possivel retornar a lista de gastos')
+    }) 
+      
   }
 
   watchMessage() {
@@ -60,13 +55,15 @@ class BotTelegram {
         this.witClient.witSendMessage(msg.text)    
           .then(response => {
             if (Object.keys(response.entities).includes('salvar')) {
-              let payload = {
+              payload = {
                 msgId: msg.from.id,
                 first_name: msg.from.first_name,
                 valor: response.entities.amount_of_money[0].value,
                 descricao: response.entities.local_search_query[0].value
               }
               this.salvar(payload)
+            } else if(Object.keys(response.entities).includes('exibir')) {
+
             }
           }).catch(e => {
             console.log(e)
